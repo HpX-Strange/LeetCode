e5b6a833336626b7046641ece7df0607d0d0a22b
Vinayak Mishra <155884774+HpX-Strange@users.noreply.github.com>
2026-02-10 10:10:50 +0530

    Time: 8 ms (76.57%) | Memory: 51.2 MB (98.28%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& state) {
-        state[node] = 1; 
 
-        for (int nbr : graph[node]) {
-            if (state[nbr] == 1) return true;         
-            if (state[nbr] == 0 && dfs(nbr, graph, state))
+    bool dfs(vector<vector<int>>& graph, int i, vector<bool> &visited, vector<bool> &one_path){
+        visited[i]=true;
+        one_path[i]=true;
+
+        for(int &nbr: graph[i]){
+            if(!visited[nbr]){
+                if(dfs(graph, nbr, visited, one_path)){
+                    return true;
+                }
+            }
+            else if(one_path[nbr]){
                 return true;
+            }
         }
-
-        state[node] = 2;
+        one_path[i]=false;
         return false;
     }
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> state(n, 0);
+        int n=graph.size();
 
-        for (int i = 0; i < n; i++) {
-            if (state[i] == 0)
-                dfs(i, graph, state);
+        vector<bool> visited(n, false);
+        vector<bool> one_path(n, false);
+        bool cycle_found=false;
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                if(dfs(graph, i, visited, one_path)){
+                    cycle_found=true;
+                }
+            }
         }
-
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            if (state[i] == 2)
+        for(int i=0; i<n; i++){
+            if(one_path[i]==false){
                 ans.push_back(i);
+            }
         }
         return ans;
     }
