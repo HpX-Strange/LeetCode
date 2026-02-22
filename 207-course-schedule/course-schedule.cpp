44ae715e01cfcafec181b6c5b8a134fc01240af8
Vinayak Mishra <155884774+HpX-Strange@users.noreply.github.com>
2026-02-22 18:23:59 +0530

    Time: 7 ms (48.23%) | Memory: 20.2 MB (27.13%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    bool topo(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree){
+        queue<int> que;
+        int count=0;
+
+        for(int i=0; i<n; i++){
+            if(indegree[i]==0){
+                que.push(i);
+                count++;
+            }
+        }
+        while(!que.empty()){
+            int curr= que.front();
+            que.pop();
+
+            for(auto& nbr: adj[curr]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0){
+                    que.push(nbr);
+                    count++;
+                }
+            }
+        }
+        if(count==n)    return true;
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(numCourses, 0);
+
+        for(auto &vec: prerequisites){
+            int a= vec[0];
+            int b= vec[1];
+
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+
+        return topo(adj, numCourses, indegree);
+
+    }
+};
