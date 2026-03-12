5cba455cbc161e591898be73766ec40c6b49f84b
Vinayak Mishra <155884774+HpX-Strange@users.noreply.github.com>
2026-03-12 16:37:41 +0530

    Time: 4 ms (68.14%) | Memory: 99.1 MB (51.49%) - LeetSync

---
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> ans;
+        vector<int> indegree(n);
+        for(int i=0; i<edges.size(); i++){
+            int v= edges[i][1];
+            indegree[v]++;
+        }
+        for(int i=0; i<n; i++){
+            if(indegree[i]==0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
