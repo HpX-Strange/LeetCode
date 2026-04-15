982ee389f4622db5cd97238a373a1adf3e0fee73
Vinayak Mishra <155884774+HpX-Strange@users.noreply.github.com>
2026-04-15 11:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (58.02%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    // int count_bit(int n){
-    //     int count=0;
-    //     while(n){
-    //         count+= (n&1);
-    //         n>>=1;
-    //     }
-    //     return count;
-    // }
+    int count_bit(int n){
+        int count=0;
+        while(n){
+            count+= (n&1);
+            n>>=1;
+        }
+        return count;
+    }
 
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for (int i=0; i<=n; i++){
-            ans.push_back(__builtin_popcount(i));
+        // vector<int> ans;
+        // for (int i=0; i<=n; i++){
+        //     ans.push_back(count_bit(i));
+        // }
+        // return ans;
+        vector<int> ans(n+1);
+        ans[0]=0;
+        for(int i=1; i<=n; i++){
+            ans[i]= ans[i>>1] + (i & 1);
         }
         return ans;
     }
